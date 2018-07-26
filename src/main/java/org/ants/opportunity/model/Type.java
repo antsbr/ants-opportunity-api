797104aa2f589a0d1107263044fe0658b05c0538
feoliv <fv.academico@gmail.com>
2018-07-26 17:24:28 -0300

    Add atributes constraints annotation into the models

--- src/main/java/org/ants/opportunity/model/Type.java
+++ src/main/java/org/ants/opportunity/model/Type.java
@@ -1,11 +1,16 @@
 package org.ants.opportunity.model;
 
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.Size;
 
 public class Type {
 
     @Id
     public String id;
+    @NotBlank
+    @Size(min = 3, max = 50)
     public String name;
 
     public Type() {}
