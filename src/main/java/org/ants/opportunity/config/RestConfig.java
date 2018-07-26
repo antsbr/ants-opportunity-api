4586a17db4157a96c9d7575c214be957a1a610d4
feoliv <fv.academico@gmail.com>
2018-07-26 17:20:44 -0300

    Add RestConfig class to configure repository validation

---
+++ src/main/java/org/ants/opportunity/config/RestConfig.java
@@ -0,0 +1,33 @@
+package org.ants.opportunity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class RestConfig extends RepositoryRestConfigurerAdapter {
+
+
+    @Autowired
+    @Lazy
+    private Validator validator;
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        validatingListener.addValidator("beforeCreate", validator);
+        validatingListener.addValidator("beforeSave", validator);
+    }
+
+    @Bean
+    public javax.validation.Validator localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
+}
+
+
