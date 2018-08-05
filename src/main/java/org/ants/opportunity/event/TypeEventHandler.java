0befa16863fdce9dfa207bfd77dcfc77d5e10879
Felipe Vianna <felipevianna45@gmail.com>
2018-08-05 19:31:13 -0300

    Add type event handler before update to change inner reference in opportunity

---
+++ src/main/java/org/ants/opportunity/event/TypeEventHandler.java
@@ -0,0 +1,28 @@
+package org.ants.opportunity.event;
+
+import org.ants.opportunity.model.Opportunity;
+import org.ants.opportunity.model.Type;
+import org.ants.opportunity.repository.OpportunityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleAfterSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler(Type.class)
+public class TypeEventHandler {
+    private final OpportunityRepository opportunities;
+
+    @Autowired
+    public TypeEventHandler(OpportunityRepository opportunities){
+        this.opportunities = opportunities;
+    }
+
+    @HandleAfterSave
+    public void updateOpportunityInfo(Type type) {
+        for ( Opportunity opportunity : opportunities.findByTypeId(type.id) ){
+            opportunity.setType(type);
+            opportunities.save(opportunity);
+        }
+    }
+}
