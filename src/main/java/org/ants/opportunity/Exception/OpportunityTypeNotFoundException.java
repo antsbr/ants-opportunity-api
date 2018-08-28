0007427c57fbc436ac349242884451c6a6d3c221
Felipe Vianna <fv.academico@gmail.com>
2018-08-27 21:46:45 -0300

    Add custom error handling to the API

---
+++ src/main/java/org/ants/opportunity/Exception/OpportunityTypeNotFoundException.java
@@ -0,0 +1,8 @@
+package org.ants.opportunity.Exception;
+
+public class OpportunityTypeNotFoundException extends RuntimeException{
+    public OpportunityTypeNotFoundException(String message)
+    {
+        super(message);
+    }
+}
