d58b849f8c6e46ab170722c787f1af32991f824d
Felipe Vianna <fv.academico@gmail.com>
2018-08-27 21:50:13 -0300

    Issue 16 implement rest error response (#19)
    * Update the type of exception raised to one more specific
    
    * Add custom error handling to the API

---
+++ src/main/java/org/ants/opportunity/config/RestErrorHandlerConfig.java
@@ -0,0 +1,30 @@
+package org.ants.opportunity.config;
+
+import org.ants.opportunity.Exception.OpportunityTypeNotFoundException;
+import org.ants.opportunity.model.ApiError;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestErrorHandlerConfig extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(OpportunityTypeNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(
+            OpportunityTypeNotFoundException ex) {
+        ApiError apiError = new ApiError(NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        apiError.setDebugMessage("Please find a valid type in /api/type");
+        return buildResponseEntity(apiError);
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
