5bf1d506cffa37d27ba7217ae8ba5a9ab85e2da0
Felipe Vianna <fv.academico@gmail.com>
2018-09-12 22:52:51 -0300

    Add cache support to the repositories methods

--- src/main/java/org/ants/opportunity/repository/OpportunityRepository.java
+++ src/main/java/org/ants/opportunity/repository/OpportunityRepository.java
@@ -2,6 +2,9 @@ package org.ants.opportunity.repository;
 
 import org.ants.opportunity.model.Opportunity;
 import org.bson.types.ObjectId;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,11 +17,26 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "Opportunity", path = "opportunity")
 public interface OpportunityRepository extends MongoRepository<Opportunity, String> {
 
+    @Cacheable({"opportunity"})
     @Query("{'type.name': ?0}")
     List<Opportunity> findByType(@Param("type") String type);
 
+    @Cacheable({"opportunity"})
     @Query("{'type.id': ?0}")
     List<Opportunity> findByTypeId(@Param("typeId") ObjectId typeId);
 
+    @Cacheable({"opportunity"})
+    List<Opportunity> findAll();
+
+    @Cacheable({"opportunity"})
+    Opportunity findById(ObjectId id);
+
+    @CachePut({"opportunity"})
+    Opportunity save(Opportunity opportunity);
+
+    @CacheEvict({"opportunity"})
+    void delete(Opportunity opportunity);
+
+    @Cacheable({"opportunity"})
     List<Opportunity> findByLocationNear(@Param("latitude-longitude") Point near, @Param("maxdistance") Distance maxDistance);
 }
