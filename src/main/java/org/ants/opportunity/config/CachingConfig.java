60cd5c6da72015fe0a4a232119383e94219f11d2
Felipe Vianna <fv.academico@gmail.com>
2018-09-14 14:35:53 -0300

    Add Redis connection information as environment variables

--- src/main/java/org/ants/opportunity/config/CachingConfig.java
+++ src/main/java/org/ants/opportunity/config/CachingConfig.java
@@ -1,38 +1,45 @@
 package org.ants.opportunity.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
 public class CachingConfig extends CachingConfigurerSupport {
+
+    @Value("${REDIS_HOSTNAME}")
+    private String hostname;
+    @Value("${REDIS_PORT}")
+    private int port;
+    @Value("${REDIS_PASSWORD}")
+    private String password;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName("127.0.0.1");
-        factory.setPort(6379);
+        factory.setPassword(password);
+        factory.setHostName(hostname);
+        factory.setPort(port);
         factory.setUsePool(true);
         return factory;
     }
 
     @Bean
     RedisTemplate<Object, Object> redisTemplate() {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object, Object>();
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
         return redisTemplate;
     }
 
     @Bean
+    @Override
     public CacheManager cacheManager() {
         return new RedisCacheManager(redisTemplate());
     }
