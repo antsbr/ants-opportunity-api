e183fc90f6b0946f0538bfc98f692d6f0a76bb5c
Felipe Vianna <fv.academico@gmail.com>
2018-09-14 17:23:59 -0300

    Set caching to be enabled only when on production

--- src/main/java/org/ants/opportunity/config/CachingConfig.java
+++ src/main/java/org/ants/opportunity/config/CachingConfig.java
@@ -6,11 +6,13 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@Profile("production")
 @EnableCaching
 public class CachingConfig extends CachingConfigurerSupport {
 
