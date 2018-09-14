4cdef35bbdde5488e4fbca05618923f73ec56da8
Felipe Vianna <fv.academico@gmail.com>
2018-09-14 14:59:35 -0300

    Issue 24 add redis support (#25)
    * Add Caching Configuration class
    
    * Add redis support libraries to gradle
    
    * Set the models as serializables
    
    * Enable cacheable repositories
    
    * Add cache support to the repositories methods
    
    * Segregate the cache repos
    
    * Remove unecessary called for enableCacheable
    
    * Add Redis connection information as environment variables

--- src/main/java/org/ants/opportunity/repository/OpportunityRepository.java
+++ src/main/java/org/ants/opportunity/repository/OpportunityRepository.java
@@ -2,6 +2,9 @@ package org.ants.opportunity.repository;
 
 import org.ants.opportunity.model.Opportunity;
 import org.bson.types.ObjectId;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,11 +17,26 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "Opportunity", path = "opportunity")
 public interface OpportunityRepository extends MongoRepository<Opportunity, String> {
 
+    @Cacheable({"opportunityByType"})
     @Query("{'type.name': ?0}")
     List<Opportunity> findByType(@Param("type") String type);
 
+    @Cacheable({"opportunityByType"})
     @Query("{'type.id': ?0}")
     List<Opportunity> findByTypeId(@Param("typeId") ObjectId typeId);
 
+    @Cacheable({"opportunityListAll"})
+    List<Opportunity> findAll();
+
+    @Cacheable({"opportunity"})
+    Opportunity findById(ObjectId id);
+
+    @CachePut({"opportunity","opportunityByLocation","opportunityListAll","opportunityByType"})
+    Opportunity save(Opportunity opportunity);
+
+    @CacheEvict({"opportunity","opportunityByLocation","opportunityListAll","opportunityByType"})
+    void delete(Opportunity opportunity);
+
+    @Cacheable({"opportunityByLocation"})
     List<Opportunity> findByLocationNear(@Param("latitude-longitude") Point near, @Param("maxdistance") Distance maxDistance);
 }
--- src/main/java/org/ants/opportunity/repository/TypeRepository.java
+++ src/main/java/org/ants/opportunity/repository/TypeRepository.java
@@ -1,11 +1,32 @@
 package org.ants.opportunity.repository;
 
+import org.ants.opportunity.model.Opportunity;
 import org.ants.opportunity.model.Type;
+import org.bson.types.ObjectId;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "Type", path = "type")
 public interface TypeRepository extends MongoRepository<Type, String> {
 
+    @Cacheable({"type"})
     Type findByName(String name);
+
+    @Cacheable({"type"})
+    List<Type> findById(ObjectId id);
+
+    @Cacheable({"type"})
+    List<Type> findAll();
+
+    @CachePut({"type"})
+    Type save(Type type);
+
+    @CacheEvict({"type"})
+    void delete(Type type);
+
 }
