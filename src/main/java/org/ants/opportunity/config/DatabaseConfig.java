fb7fd70a851302eccbdf728d60546b3653b605c0
Felipe Vianna <fv.academico@gmail.com>
2018-10-08 23:11:24 +0100

    Add method to ensure index creation when local database

--- src/main/java/org/ants/opportunity/config/DatabaseConfig.java
+++ src/main/java/org/ants/opportunity/config/DatabaseConfig.java
@@ -10,15 +10,25 @@ import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
+import org.ants.opportunity.model.Opportunity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeospatialIndex;
+
 import java.io.IOException;
 
 @Configuration
 public class DatabaseConfig extends AbstractMongoConfiguration {
+    private static final String IP = "127.0.0.1";
+    private static final int PORT = 27017;
+    private static MongoClient mongoClient;
+    private static MongodExecutable mongodExecutable;
+    private static MongoTemplate mongoTemplate;
 
     @Value("${MONGO_URI}")
     private String mongoUri;
@@ -29,34 +39,47 @@ public class DatabaseConfig extends AbstractMongoConfiguration {
 
     @Bean("mongo")
     @Profile("production")
-    public Mongo getMongoConnection() {
+    public Mongo mongo() {
         return new MongoClient(new MongoClientURI(mongoUri));
     }
 
     @Bean("mongo")
     @Profile({"!production","!test"})
-    public Mongo mongo(){
-        String ip = "localhost";
-        int port = 27017;
+    public Mongo getLocalMongoSetup(){
+        startLocalDatabase();
+        connectToLocalDatabase();
+        ensureIndexCreation();
+        return mongoClient;
+    }
 
+    private void startLocalDatabase(){
         IMongodConfig mongodConfig = null;
         try {
             mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
-                    .net(new Net(ip, port, Network.localhostIsIPv6()))
+                    .net(new Net(IP, PORT, Network.localhostIsIPv6()))
                     .build();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         MongodStarter starter = MongodStarter.getDefaultInstance();
-        MongodExecutable mongodExecutable = starter.prepare(mongodConfig);
+        mongodExecutable = starter.prepare(mongodConfig);
 
         try {
             mongodExecutable.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new MongoClient(ip, port);
+    }
+
+    private void ensureIndexCreation(){
+        mongoTemplate = new MongoTemplate(mongoClient, "ants");
+        //Ensure Opportunity Location Geospatial Index
+        mongoTemplate.indexOps(Opportunity.class).ensureIndex( new GeospatialIndex("location").typed(GeoSpatialIndexType.GEO_2DSPHERE) );
+    }
+
+    private void connectToLocalDatabase(){
+        mongoClient = new MongoClient(IP, PORT);
     }
 
     @Override
