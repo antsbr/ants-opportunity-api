4f330f8b71dfb7b4a416d536d45da172be8956a2
Felipe Vianna <fv.academico@gmail.com>
2018-10-08 22:11:33 +0100

    Isolate the fake data creation

--- src/test/java/OpportunityTest/OpportunityFindTest.java
+++ src/test/java/OpportunityTest/OpportunityFindTest.java
@@ -1,5 +1,12 @@
+package OpportunityTest;
+
+import Connection.DatabaseConnection;
+import DataLoader.OpportunityLoader;
+import DataLoader.TypeLoader;
+import com.github.javafaker.Faker;
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBObject;
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import de.flapdoodle.embed.mongo.MongodExecutable;
 import de.flapdoodle.embed.mongo.MongodStarter;
@@ -8,64 +15,71 @@ import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.ants.opportunity.model.Opportunity;
+import org.ants.opportunity.model.Type;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class OpportunityFindTest {
-    private MongodExecutable mongodExecutable;
-    private MongoTemplate mongoTemplate;
+    private static final String API_SEARCH_OPPORTUNITY_BASE = "/api/opportunity/search/";
+    private static final String FIND_BY_LOCATION_NEAR = "findByLocationNear";
 
-    @AfterEach
-    void clean() {
+    @BeforeAll
+    static void loadData(){
+        TypeLoader.addFakeType(10);
+        OpportunityLoader.addFakeOpportunity(100);
+    }
+
+/*    @AfterAll
+    static void clean(){
+        mongoTemplate.dropCollection("opportunity");
+        mongoTemplate.dropCollection("type");
+    }
+
+ /*   @AfterAll
+    static void clean() {
         mongodExecutable.stop();
     }
 
-    @BeforeEach
-    void setup() throws Exception {
-        String ip = "localhost";
-        int port = 27017;
-
-        IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
-                .net(new Net(ip, port, Network.localhostIsIPv6()))
-                .build();
-
-        MongodStarter starter = MongodStarter.getDefaultInstance();
-        mongodExecutable = starter.prepare(mongodConfig);
-        mongodExecutable.start();
-        mongoTemplate = new MongoTemplate(new MongoClient(ip, port), "ants");
-    }
-
-    @DisplayName("given object to save"
+ /*   @DisplayName("given object to save"
             + " when save object using MongoDB template"
             + " then object is saved")
     @Test
     void test() throws Exception {
-        // given
+        /* given
         DBObject objectToSave = BasicDBObjectBuilder.start()
                 .add("key", "value")
                 .get();
 
-        // when
-        mongoTemplate.save(objectToSave, "collection");
-
-        /* then */
-        assert(1==1);
+        //assert(1==1);
         //assert(mongoTemplate.findAll(DBObject.class, "collection")).get(0).equals("value");
-    }
+    }*/
 
     @Test public void
-    lotto_resource_returns_200_with_expected_id_and_winners() {
-
-        when().
-                get("/api/opportunity/5b89efe5b4462c0c1c7b655c").
+    findOpportunityByLocationNearRadius() {
+        given().
+                param("latitude-longitude","2.435048,48.886112").
+                param("maxdistance","20km").
+                when().
+                get(API_SEARCH_OPPORTUNITY_BASE+FIND_BY_LOCATION_NEAR).
                 then().
-                statusCode(200).
-                body("name", equalTo("Teste2"));
+                statusCode(404);/*.
+        body("name", equalTo("Teste2"));*/
 
     }
 
