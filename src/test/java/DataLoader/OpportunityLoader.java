2bae0726ce53b6f74d5740eb33b977bbf31fc5f3
Felipe Vianna <fv.academico@gmail.com>
2018-10-09 23:27:11 +0100

    Add remove all elements method

--- src/test/java/DataLoader/OpportunityLoader.java
+++ src/test/java/DataLoader/OpportunityLoader.java
@@ -13,27 +13,46 @@ import java.util.Random;
 public class OpportunityLoader {
     private static final MongoTemplate DATABASE = DatabaseConnection.getIntance();
     private static final Faker FAKER = new Faker();
+    private static final String OPPORTUNITY_COLLECTION = "opportunity";
 
     public static void addFakeElement(int numberOfOpportunities){
-        int numberOfTypesAvailable = (int)DATABASE.count(new Query(), Type.class);
         for(int i = 0;i<numberOfOpportunities; i++) {
-            // Select a random type for each opportunity
-            Query query = new Query();
-            query.limit(-1);
-            query.skip(new Random().nextInt(numberOfTypesAvailable));
-            Type type = DATABASE.find(query, Type.class).get(0);
-            // Creates the opportunity
-            Opportunity opportunity = new Opportunity();
-            opportunity.setName(FAKER.beer().name());
-            opportunity.setType(type);
-            opportunity.setLocation(new GeoJsonPoint(Double.parseDouble(FAKER.address().longitude()),
-                                                    Double.parseDouble(FAKER.address().latitude())
-                                                    ));
-            DATABASE.save(opportunity, "opportunity");
+            DATABASE.save(getFakeElement(), OPPORTUNITY_COLLECTION);
         }
     }
 
     public static void addElement(Opportunity opportunity){
-        DATABASE.save(opportunity, "opportunity");
+        DATABASE.save(opportunity, OPPORTUNITY_COLLECTION);
+    }
+
+    public static Opportunity getFakeElement(){
+        Opportunity opportunity = createFakeElement();
+        DATABASE.insert(opportunity);
+        return opportunity;
+    }
+
+    public static void removeElement(Opportunity opportunity){
+        DATABASE.remove(opportunity);
+    }
+
+    public static void removeAllElements(){
+        DATABASE.remove(new Query(),OPPORTUNITY_COLLECTION);
+    }
+
+    private static Opportunity createFakeElement(){
+        int numberOfTypesAvailable = (int)DATABASE.count(new Query(), Type.class);
+        // Select a random type for each opportunity
+        Query query = new Query();
+        query.limit(-1);
+        query.skip(new Random().nextInt(numberOfTypesAvailable));
+        Type type = DATABASE.find(query, Type.class).get(0);
+        // Creates the opportunity
+        Opportunity opportunity = new Opportunity();
+        opportunity.setName(FAKER.beer().name());
+        opportunity.setType(type);
+        opportunity.setLocation(new GeoJsonPoint(Double.parseDouble(FAKER.address().longitude()),
+                Double.parseDouble(FAKER.address().latitude())
+        ));
+        return opportunity;
     }
 }
--- src/test/java/DataLoader/TypeLoader.java
+++ src/test/java/DataLoader/TypeLoader.java
@@ -4,21 +4,41 @@ import Connection.DatabaseConnection;
 import com.github.javafaker.Faker;
 import org.ants.opportunity.model.Type;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 
 public class TypeLoader {
     private static final MongoTemplate DATABASE = DatabaseConnection.getIntance();
     private static final Faker FAKER = new Faker();
+    private static final String TYPE_COLLECTION = "type";
 
     public static void addFakeElement(int numberOfTypes){
         for(int i = 0;i < numberOfTypes; i++) {
-            Type type = new Type();
-            type.setName(FAKER.funnyName().name());
-            DATABASE.save(type, "type");
+            DATABASE.save(createFakeElement(), TYPE_COLLECTION);
         }
     }
 
     public static void addElement(Type type){
-        DATABASE.save(type, "type");
+        DATABASE.save(type, TYPE_COLLECTION);
+    }
+
+    public static Type getFakeElement(){
+        Type type = createFakeElement();
+        DATABASE.insert(type);
+        return type;
+    }
+
+    public static void removeElement(Type type){
+        DATABASE.remove(type);
+    }
+
+    public static void removeAllElements(){
+        DATABASE.remove(new Query(),TYPE_COLLECTION);
+    }
+
+    private static Type createFakeElement(){
+        Type type = new Type();
+        type.setName(FAKER.funnyName().name());
+        return type;
     }
 
 }
